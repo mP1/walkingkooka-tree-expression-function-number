85d8b33aa7d805b31c328787812e68d846bf077f
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2022-02-03 23:12:44 +1100

    fixed function()
    - Closes https://github.com/mP1/walkingkooka-tree-expression-function-number/issues/127
    - fixed

---
+++ src/main/java/walkingkooka/tree/expression/function/number/StringExpressionFunctionFixed.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2020 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.tree.expression.function.number;
+
+import walkingkooka.Cast;
+import walkingkooka.collect.list.Lists;
+import walkingkooka.tree.expression.ExpressionNumber;
+import walkingkooka.tree.expression.function.ExpressionFunctionContext;
+import walkingkooka.tree.expression.function.ExpressionFunctionParameter;
+import walkingkooka.tree.expression.function.ExpressionFunctionParameterName;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.List;
+
+// https://exceljet.net/excel-functions/excel-fixed-function
+final class StringExpressionFunctionFixed<C extends ExpressionFunctionContext> extends StringExpressionFunction<C> {
+
+    /**
+     * Instance getter.
+     */
+    static <C extends ExpressionFunctionContext> StringExpressionFunctionFixed<C> instance() {
+        return Cast.to(INSTANCE);
+    }
+
+    /**
+     * Singleton
+     */
+    private static final StringExpressionFunctionFixed<?> INSTANCE = new StringExpressionFunctionFixed<>();
+
+    /**
+     * Private ctor
+     */
+    private StringExpressionFunctionFixed() {
+        super("fixed");
+    }
+
+    @Override
+    public List<ExpressionFunctionParameter<?>> parameters() {
+        return PARAMETERS;
+    }
+
+    private final static ExpressionFunctionParameter<ExpressionNumber> NUMBER = ExpressionFunctionParameter.NUMBER;
+
+    private final static ExpressionFunctionParameter<ExpressionNumber> DECIMALS = ExpressionFunctionParameterName.with("decimals")
+            .optional(ExpressionNumber.class);
+
+    private final static ExpressionFunctionParameter<Boolean> COMMAS = ExpressionFunctionParameterName.with("commas")
+            .optional(Boolean.class);
+
+    private final static List<ExpressionFunctionParameter<?>> PARAMETERS = Lists.of(
+            NUMBER,
+            DECIMALS,
+            COMMAS
+    );
+
+    // https://github.com/apache/poi/blob/7eaca60a1a364ce6e232363d27823e971a992705/poi/src/main/java/org/apache/poi/ss/formula/functions/Roman.java
+
+    @Override
+    public String apply(final List<Object> parameters,
+                        final C context) {
+        this.checkParameterCount(parameters);
+
+        final ExpressionNumber number = NUMBER.getOrFail(parameters, 0);
+        final int decimals = DECIMALS.get(parameters, 1)
+                .orElseGet(() -> context.expressionNumberKind().create(2))
+                .intValueExact();
+        final Boolean commas = COMMAS.get(parameters, 2).orElse(false);
+
+        final DecimalFormatSymbols symbols = new DecimalFormatSymbols();
+
+        symbols.setMinusSign(context.negativeSign());
+
+        symbols.setDecimalSeparator(context.decimalSeparator());
+        symbols.setGroupingSeparator(context.groupingSeparator());
+
+
+        final DecimalFormat format = new DecimalFormat("", symbols);
+        format.setRoundingMode(context.mathContext().getRoundingMode());
+
+        format.setMaximumFractionDigits(decimals);
+        format.setMinimumFractionDigits(decimals);
+
+        format.setDecimalSeparatorAlwaysShown(decimals > 0);
+
+        format.setGroupingUsed(!commas);
+
+        return format.format(number.bigDecimal()); // TODO should test if BigDecimal or Double
+    }
+}
---
+++ src/test/java/walkingkooka/tree/expression/function/number/StringExpressionFunctionFixedTest.java
@@ -0,0 +1,177 @@
+/*
+ * Copyright 2022 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.tree.expression.function.number;
+
+import org.junit.jupiter.api.Test;
+import walkingkooka.Cast;
+import walkingkooka.collect.list.Lists;
+import walkingkooka.tree.expression.function.ExpressionFunctionContext;
+
+public final class StringExpressionFunctionFixedTest extends StringExpressionFunctionTestCase<StringExpressionFunctionFixed<ExpressionFunctionContext>, String> {
+
+    @Test
+    public void testOnlyNumber() {
+        this.fixedAndCheck(
+                4,
+                "4d00"
+        );
+    }
+
+    @Test
+    public void testNegativeOnlyNumber() {
+        this.fixedAndCheck(
+                -4,
+                "n4d00"
+        );
+    }
+
+    @Test
+    public void testOnlyNumberDecimal() {
+        this.fixedAndCheck(
+                4.5,
+                "4d50"
+        );
+    }
+
+    @Test
+    public void testOnlyNumberDecimal3() {
+        this.fixedAndCheck(
+                4.123,
+                "4d12"
+        );
+    }
+
+    private void fixedAndCheck(final Number value,
+                               final String fixed) {
+        this.applyAndCheck2(
+                Lists.of(
+                        KIND.create(value)
+                ),
+                fixed
+        );
+    }
+
+    @Test
+    public void test1_3() {
+        this.fixedAndCheck(
+                1,
+                3,
+                "1d000"
+        );
+    }
+
+    @Test
+    public void test1000_2() {
+        this.fixedAndCheck(
+                1000,
+                2,
+                "1g000d00"
+        );
+    }
+
+    @Test
+    public void test1000_3() {
+        this.fixedAndCheck(
+                1000,
+                3,
+                "1g000d000"
+        );
+    }
+
+    @Test
+    public void test1000_1() {
+        this.fixedAndCheck(
+                1000,
+                1,
+                "1g000d0"
+        );
+    }
+
+    @Test
+    public void test1000_1Rounded() {
+        this.fixedAndCheck(
+                1000.45,
+                1,
+                "1g000d5"
+        );
+    }
+
+    private void fixedAndCheck(final Number value,
+                               final Number decimals,
+                               final String fixed) {
+        this.applyAndCheck2(
+                Lists.of(
+                        KIND.create(value),
+                        KIND.create(decimals)
+                ),
+                fixed
+        );
+    }
+
+    @Test
+    public void test1000_1_False() {
+        this.fixedAndCheck(
+                1000.45,
+                1,
+                false,
+                "1g000d5"
+        );
+    }
+
+    @Test
+    public void test1000_1_True() {
+        this.fixedAndCheck(
+                1000.45,
+                1,
+                true,
+                "1000d5"
+        );
+    }
+
+    private void fixedAndCheck(final Number value,
+                               final Number decimals,
+                               final boolean suppressSeparators,
+                               final String fixed) {
+        this.applyAndCheck2(
+                Lists.of(
+                        KIND.create(value),
+                        KIND.create(decimals),
+                        suppressSeparators
+                ),
+                fixed
+        );
+    }
+
+    @Test
+    public void testToString() {
+        this.toStringAndCheck(
+                this.createBiFunction(),
+                "fixed"
+        );
+    }
+
+    @Override
+    public StringExpressionFunctionFixed<ExpressionFunctionContext> createBiFunction() {
+        return StringExpressionFunctionFixed.instance();
+    }
+
+    @Override
+    public Class<StringExpressionFunctionFixed<ExpressionFunctionContext>> type() {
+        return Cast.to(StringExpressionFunctionFixed.class);
+    }
+}
