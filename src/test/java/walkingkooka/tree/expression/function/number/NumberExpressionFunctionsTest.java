f9f6093e89c9b4261f006c92c6e6b03e61e619e4
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-09-18 16:37:03 +1000

    https://github.com/mP1/walkingkooka/pull/3326 PublicStaticHelperTesting extends PublicClassTesting
    - https://github.com/mP1/walkingkooka/pull/3326
    - PublicStaticHelperTesting extends PublicClassTesting

--- src/test/java/walkingkooka/tree/expression/function/number/NumberExpressionFunctionsTest.java
+++ src/test/java/walkingkooka/tree/expression/function/number/NumberExpressionFunctionsTest.java
@@ -19,7 +19,6 @@ package walkingkooka.tree.expression.function.number;
 
 import org.junit.jupiter.api.Test;
 import walkingkooka.collect.set.SortedSets;
-import walkingkooka.reflect.JavaVisibility;
 import walkingkooka.reflect.PublicStaticHelperTesting;
 import walkingkooka.text.CaseSensitivity;
 import walkingkooka.tree.expression.function.ExpressionFunction;
@@ -68,10 +67,5 @@ public final class NumberExpressionFunctionsTest implements PublicStaticHelperTe
     @Override
     public boolean canHavePublicTypes(final Method method) {
         return false;
-    }
-
-    @Override
-    public JavaVisibility typeVisibility() {
-        return JavaVisibility.PUBLIC;
     }
 }
